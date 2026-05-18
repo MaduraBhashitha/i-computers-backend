dc5a866854f5e033a4d7cd3a8d1e4650157a100a
MaduraBhashitha <madurabashitha12@gmail.com>
2026-05-18 23:49:13 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import studentRouter from './routes/studentRouter.js'
+import userRouter from './routes/userRouter.js'
+import authenticateUser from './middlewares/authenticate.js'
+import productRouter from './routes/productRouter.js'
+
+const mongoUri = "mongodb+srv://admin:1234@cluster0.amjxnj9.mongodb.net/?appName=Cluster0"
+
+mongoose.connect(mongoUri).then(
+    ()=>{
+        console.log("Connected to MongoDB")
+    }
+)
+
+
+const app = express()
+
+app.use( express.json() )
+
+
+//Authentication Part
+app.use(authenticateUser)
+
+app.use("/students", studentRouter)
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+ 
+app.listen( 3000 ,
+    ()=>{
+      console.log("Server is running!")  
+    }
+)
