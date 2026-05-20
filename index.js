7f7db08b699bf28371f73bf473099bf17fbf68c7
MaduraBhashitha <madurabashitha12@gmail.com>
2026-05-20 17:22:32 +0530

    securing the secret variable

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
 import userRouter from './routes/userRouter.js'
 import authenticateUser from './middlewares/authenticate.js'
 import productRouter from './routes/productRouter.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const mongoUri = "mongodb+srv://admin:1234@cluster0.amjxnj9.mongodb.net/?appName=Cluster0"
+const mongoUri = process.env.MONGO_URI
 
 mongoose.connect(mongoUri).then(
     ()=>{
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+dotenv.config()
 export default function authenticateUser(req, res, next){
 
     const header = req.header("Authorization")
@@ -8,7 +10,7 @@ export default function authenticateUser(req, res, next){
 
     console.log(token)
 
-    jwt.verify(token , "comp99#12@", 
+    jwt.verify(token , process.env.JWT_SECRET, 
         (err , decoded)=>{
             
             if(decoded == null){
